b6c1efdc03a92fbd29d003be1f34d270b2ed918d
Noble Obioma <nobioma1@gmail.com>
2019-06-10 16:33:53 +0100

    create routes to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+    </div>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+  </Router>
 );
 
 export default App;
